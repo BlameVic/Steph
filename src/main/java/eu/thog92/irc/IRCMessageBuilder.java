b1efb3fbec3412e856cc948ee6020c67a6d129c2
RX14 <chris@rx14.co.uk>
2014-11-02 14:12:10 +0000

    Some quick ideas for the MessageBuilder

--- src/main/java/eu/thog92/irc/IRCMessageBuilder.java
+++ src/main/java/eu/thog92/irc/IRCMessageBuilder.java
@@ -4,4 +4,7 @@ package eu.thog92.irc;
  * Created by rx14 on 31/10/14.
  */
 public class IRCMessageBuilder {
+    /*public boolean sendToClient(IRCClient client) {
+        return client.writeLine();
+    }*/
 }

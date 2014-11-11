ef325bfe9e1b97d709b3201cf09065e5f64fafb7
RX14 <chris@rx14.co.uk>
2014-11-11 16:25:26 +0000

    spec out the IRCMessageSender class

--- src/main/java/eu/thog92/irc/IRCMessageSender.java
+++ src/main/java/eu/thog92/irc/IRCMessageSender.java
@@ -7,9 +7,27 @@ public class IRCMessageSender {
         this.client = client;
     }
 
-    public void privateMessage(String message, String destination) {
-        String PRIVMSG = "PRIVMSG " + destination + " :" + message;
+    public boolean command(String... sends) {
+        return client.writeLine(String.join(" ", sends));
+    }
 
-        client.writeLine(PRIVMSG);
+    public boolean privateMessage(String message, String destination) {
+        return command("PRIVMSG", destination, ":" + message);
+    }
+
+    public boolean join(String... channels) {
+        return command("JOIN", String.join(",", channels));
+    }
+
+    public boolean join(String channel, String key) {
+        return command("JOIN", channel, key);
+    }
+
+    public boolean part(String... channels) {
+        return command("PART", String.join(",", channels));
+    }
+
+    public boolean part(String channel, String reason) {
+        return command("PART", channel, ":" + reason);
     }
 }

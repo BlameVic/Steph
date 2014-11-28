86feac5471e2d527e626699df6b1bb6fd0e3d127
RX14 <chris@rx14.co.uk>
2014-11-28 21:53:01 +0000

    Removed old handlers

--- src/main/java/com/github/blamevic/steph/common/handlers/AbstractHandlerBase.java
+++
@@ -1,1 +0,0 @@
-package com.github.blamevic.steph.common.handlers;
--- src/main/java/com/github/blamevic/steph/common/handlers/cmd/ICommand.java
+++
@@ -1,9 +0,0 @@
-package com.github.blamevic.steph.common.handlers.cmd;
-
-public interface ICommand
-{
-
-    public String getCommandName();
-
-    public void processCommand(String username, String[] args);
-}

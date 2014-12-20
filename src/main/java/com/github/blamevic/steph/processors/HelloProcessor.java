41a3d7453b4bd1e0002943729e05820975f74722
RX14 <chris@rx14.co.uk>
2014-12-20 22:45:00 +0000

    Fix HelloProcessor

--- src/main/java/com/github/blamevic/steph/processors/HelloProcessor.java
+++ src/main/java/com/github/blamevic/steph/processors/HelloProcessor.java
@@ -3,17 +3,18 @@ package com.github.blamevic.steph.processors;
 import com.github.blamevic.event.EventProcessorBase;
 import com.github.blamevic.steph.common.ChatEvent;
 import com.github.blamevic.event.IEvent;
+import com.github.blamevic.steph.matchers.MessageContentMatcher;
 
 public class HelloProcessor extends EventProcessorBase
 {
     public HelloProcessor()
     {
-        //addEventMatcher(new ContainsMatcher("Hey Steph!"));
+        addEventMatcher(new MessageContentMatcher("Hey Steph!"));
     }
 
     @Override
     public void processEvent(IEvent event)
     {
-        //event.sendMessage("Hey!");
+        ((ChatEvent)event).sendMessage("Hey!");
     }
 }
